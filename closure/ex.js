191ae4e6c0d5cc0df58c6a7ba033e4f7ee9c7631
ddijk <dick@dijkrosoft.nl>
2019-12-09 10:06:45 +0100

    solution that supports max 3 chained calls

--- closure/ex.js
+++ closure/ex.js
@@ -1,8 +1,32 @@
 "use strict";
 
-function strBuilder(str) {
-	return strBuilder;
+function strBuilder(greeting) {
+
+	function b(x) {
+
+		if (x == undefined) {
+			return greeting;
+		}
+
+
+		// c
+		return function(y) {
+			if ( y == undefined) {
+				return greeting+x;
+			}
+
+			return function(z) {
+				if ( z == undefined) {
+					return greeting+x+y;
+				}
+					return greeting+x+y;
+
+			}
+		}
+	}
+	return b;
 }
+
 
 var hello = strBuilder("Hello, ");
 var kyle = hello("Kyle");
@@ -10,9 +34,12 @@ var susan = hello("Susan");
 var question = kyle("?")();
 var greeting = susan("!")();
 
-console.log(strBuilder("Hello, ")("")("Kyle")(".")("")() === "Hello, Kyle.");
+// console.log(strBuilder("Hello, ")("")("Kyle")(".")("")() === "Hello, Kyle.");
+console.log(hello());
 console.log(hello() === "Hello, ");
 console.log(kyle() === "Hello, Kyle");
+console.log(kyle());
 console.log(susan() === "Hello, Susan");
+console.log(susan());
 console.log(question === "Hello, Kyle?");
 console.log(greeting === "Hello, Susan!");
